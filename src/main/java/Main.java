a3a6e9c24cb515c904c649e0d44c73fa224e8d55
Davide Marchetti <davide.marchetti.mail@gmail.com>
2024-06-28 00:21:44 +0200

    feat(stage3): add REPL

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-         System.out.print("$ ");
+        while (true) {
+            System.out.print("$ ");
 
-         Scanner scanner = new Scanner(System.in);
-         String input = scanner.nextLine();
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
 
-         System.out.printf("%s: command not found%n", input);
+            System.out.printf("%s: command not found%n", input);
+        }
     }
 }
